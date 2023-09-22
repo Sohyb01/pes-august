9feaf029571704e51545f60924254212f1c345a3
Sohyb01 <sohyb0155@gmail.com>
2023-09-22 18:22:29 +0300

    Fixed an import error, removed old tables from dashboard

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import prisma from "../lib/prisma";
-import UploadCertificateForm from "../Components/UploadCertificateForm";
+import prisma from "@/app/lib/prisma";
+import UploadCertificateForm from "@/app/Components/UploadCertificateForm";
 
 async function Dashboard() {
-  const parentApplicationData = await prisma.parentApplicationData.findMany({});
-  const studentApplicationData = await prisma.studentApplicationData.findMany(
+  const pesKidsApplicationData = await prisma.pesKidsApplicationData.findMany(
+    {}
+  );
+  const pesYouthApplicationData = await prisma.pesYouthApplicationData.findMany(
     {}
   );
   const CertificateData = await prisma.certificate.findMany({});
@@ -36,90 +38,6 @@ async function Dashboard() {
   return (
     <div className="flex flex-col items-center w-full min-h-[100vh] pt-[144px] ">
       <section className="flex flex-col items-center w-full pb-[72px] section__styles gap-16 ">
-        {/* Parents applications (kids) Container */}
-        <div className="flex flex-col w-full items-center text-start gap-4">
-          <h1 className="text-blue-900 text-start w-full">
-            Parent Applications (kids)
-          </h1>
-          <div className="flex flex-col w-full p-5 bg-white shadow-effect">
-            <div className="overflow-x-auto scroll-styling">
-              <table className="table table-xs table-pin-rows table-pin-cols bg-white scroll-styling">
-                <thead>
-                  <tr className="flex gap-2 bg-white">
-                    <td>Application Date</td>
-                    <td>Parent Email</td>
-                    <td>Parent Mobile #</td>
-                    <td>Student Name</td>
-                    <td>Student Age</td>
-                    <td>Student Gender</td>
-                    <td>Student Mobile #</td>
-                    <td>Student has Laptop/PC?</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {parentApplicationData.map((application, key) => {
-                    return (
-                      <tr key={key} className="flex gap-2 bg-white">
-                        <td>
-                          {improveDateFormat(application.createdAt.toString())}
-                        </td>
-                        <td>{application.parentEmail}</td>
-                        <td>{application.parentMobile}</td>
-                        <td>{application.studentName}</td>
-                        <td>{application.studentAge}</td>
-                        <td>{application.studentGender}</td>
-                        <td>{application.studentMobile}</td>
-                        <td>{application.studentHasLaptop}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-        {/* University Student applications */}
-        <div className="flex flex-col w-full items-center text-start gap-4">
-          <h1 className="text-blue-900 text-start w-full">
-            University Students Applications
-          </h1>
-          <div className="flex flex-col w-full p-5 bg-white shadow-effect">
-            <div className="overflow-x-auto scroll-styling">
-              <table className="table table-xs table-pin-rows table-pin-cols bg-white scroll-styling">
-                <thead>
-                  <tr className="flex gap-2 bg-white">
-                    <td>Application Date</td>
-                    <td>Name</td>
-                    <td>University</td>
-                    <td>Faculty</td>
-                    <td>Academic Year</td>
-                    <td>Email</td>
-                    <td>Mobile #</td>
-                    <td>Additional Message</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {studentApplicationData.map((application, key) => {
-                    return (
-                      <tr key={key} className="flex gap-2 bg-white">
-                        <td>
-                          {improveDateFormat(application.createdAt.toString())}
-                        </td>
-                        <td>{application.studentName}</td>
-                        <td>{application.studentUniversity}</td>
-                        <td>{application.studentFaculty}</td>
-                        <td>{application.studentAcademicYear}</td>
-                        <td>{application.studentEmail}</td>
-                        <td>{application.studentMobile}</td>
-                        <td>{application.studentAdditionalMessage}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
         {/* Certificates Container */}
         <div className="flex flex-col w-full items-center text-start gap-4">
           <div className="flex items-center justify-start w-full gap-8">
